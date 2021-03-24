56c5351a19b2755bf1e31ee246fa7c70c98d329e
Ritvik Mahesh <72755004+ritvik0012@users.noreply.github.com>
2021-03-24 13:54:53 +0530

    Delete users.js

--- models/users.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-
-const users = new mongoose.Schema({
-    email: String,
-    password: String,
-});
-
-module.exports = mongoose.models.users || mongoose.model("users",users);
