46be76eb40c9a364842fe7f8c28b4d1300edef27
jason krithik kumar <52494778+jkk2000@users.noreply.github.com>
2021-04-19 02:43:48 +0530

    Delete keys.js

--- helpers/keys.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    google: {
-        clientID: "764464265304-0qu6ml8t63f48dapf39000n3rajdf4dj.apps.googleusercontent.com",
-        clientSecret: "zzvbbR__eSj4FoY00drZ2PvJ"
-    },
-    session: {
-        cookieKey: "Thismyfriendisbrillant"
-    }
-};
