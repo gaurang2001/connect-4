316b454dc221be7c3872acf86e32db9b4581a3fb
“ritvik0012” <ritvik0012@gmail.com>
2021-04-20 12:47:15 +0530

    added username and change password feature

--- models/users.js
+++ models/users.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const users = new mongoose.Schema({
     email: String,
     password: String,
+    username: String,
 });
 
 module.exports = mongoose.models.users || mongoose.model("users",users);
