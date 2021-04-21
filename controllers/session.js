01f81182946a9a98e06d5eaa62f2c4bd9bff00ef
Rakshita Varadarajan <rakshitajps@gmail.com>
2021-04-21 23:44:21 +0300

    UI & Validation

--- controllers/session.js
+++ controllers/session.js
@@ -17,7 +17,7 @@ exports.getChangepass = (req, res) => {
 
 exports.postLogin = async (req, res) => {
     const email = req.body.email;
-    const password = req.body.password;
+    const password = req.body.your_pass;
     var username;
     const doesUserExists = await User.findOne({ email });
 
@@ -60,9 +60,12 @@ exports.postLogin = async (req, res) => {
 }
 
 exports.postRegister = async (req, res) => {
-    const { email, password , username } = req.body;
 
-    if (!email || !password) {
+    const email = req.body.email;
+    const password = req.body.pass;
+    const username = req.body.uname;
+
+    if (!email || !password || !username) {
         req.flash("alert", "Please enter all the fields");
         res.redirect("/register");
     }
---
+++ public/JS/register.js
@@ -0,0 +1,62 @@
+//Code for data validation
+function usernamevalidation(field){
+      var usern = field.value;
+      var pass_msg = document.getElementById("unamevalidation");
+      if (usern === "") {
+        pass_msg.innerText = "Username is required.";
+        return false;
+      }
+      else if (usern.match(/^[_a-zA-Z][_a-zA-Z0-9]{5}([_a-zA-Z0-9]{6})?/)==null){
+         pass_msg.innerText="Username has to start with _ or alphabets, and be between 6 to 12 characters with alphanumerics!";
+         return false;
+       }
+         else{
+          pass_msg.innerText="";
+          return true;
+         }
+
+}
+
+function passwordvalidation(field){
+			var pass = field.value;
+			var pass_msg = document.getElementById("pwdcheck");
+			if (pass === "") {
+				pass_msg.innerText = "Password is required.";
+        return false;
+			}
+			else if (pass.match("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})")==null){
+		     pass_msg.innerText="The password must be eight characters or longer, and have at least 1 lowercase alphabetical character, at least 1 uppercase alphabetical character and at least 1 numeric character";
+         return false;}
+		     else{
+		     	pass_msg.innerText="";
+          return true;
+		     }
+
+}
+
+function passwordsmatching(field){
+      var pwd = field.value;
+      var pass_msg = document.getElementById("pwdsmatch");
+      if (pwd === "") {
+        pass_msg.innerText = "Password confirmation is required.";
+        return false;
+      }
+      else if (pwd.localeCompare(field.form.pass.value)!=0){
+         pass_msg.innerText="Passwords don't match!";
+         return false;}
+         else{
+          pass_msg.innerText="";
+          return true;
+         }
+
+}
+
+//Form validation
+function check(form){
+    
+    
+
+   return usernamevalidation(form.uname)&&passwordvalidation(form.pass)&&passwordsmatching(form.re_pass);
+
+}
+
