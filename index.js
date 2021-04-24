df86d176c1f90f3d6d5fdc6463fab0e0f060ff91
Rakshita Varadarajan <54491584+rak108@users.noreply.github.com>
2021-04-24 19:44:38 +0530

    Update Profile, Consistency, Demo (#17)

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ const gameRoutes = require("./routes/game");
 const gameLogic = require("./controllers/game");
 const users = require("./models/users");
 
-app.set("view engine","ejs");
-app.set("views","views");
+app.set("view engine", "ejs");
+app.set("views", "views");
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(cookieParser("secret"));
@@ -54,12 +54,12 @@ app.use("/", sessionRoutes);
 app.use("/play", gameRoutes);
 
 function generateHash(length) {
-	var haystack = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-		output = "";
-	for(var i = 0; i < length; i++) {
-		output += haystack.charAt(Math.floor(Math.random() * haystack.length));
-	}
-	return output;
+    var haystack = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        output = "";
+    for (var i = 0; i < length; i++) {
+        output += haystack.charAt(Math.floor(Math.random() * haystack.length));
+    }
+    return output;
 }
 
 mongoose.connect("mongodb://localhost:27017/usersDB", {
@@ -71,13 +71,13 @@ mongoose.connect("mongodb://localhost:27017/usersDB", {
     const io = socketIo(server);
     var rooms = [];
 
-    io.on("connection", function(socket) {
+    io.on("connection", function (socket) {
         console.log("New client connected. ID: ", socket.id);
-        socket.on("join", function(data){
-            if(data.room in gameLogic.games) {
+        socket.on("join", function (data) {
+            if (data.room in gameLogic.games) {
                 var game = gameLogic.games[data.room];
-                if(typeof game.player2 != "undefined") return;
-                if(game.player1.email === socket.handshake.query.email) {
+                if (typeof game.player2 != "undefined") return;
+                if (game.player1.email === socket.handshake.query.email) {
                     socket.emit("not_allowed");
                     return;
                 }
@@ -93,43 +93,43 @@ mongoose.connect("mongodb://localhost:27017/usersDB", {
                 game.player2 = socket;
                 socket.opponent = game.player1;
                 game.player1.opponent = socket;
-                socket.emit('assign', {pid: socket.pid, hash: socket.hash});
+                socket.emit('assign', { pid: socket.pid, hash: socket.hash });
                 game.turn = 1;
                 socket.broadcast.to(data.room).emit("start");
                 io.in(socket.room).emit("assign_names", { player1: game.player1.username, player2: game.player2.username });
-                console.log("Player 2 ", socket.username ," has joined room: ", socket.room);
+                console.log("Player 2 ", socket.username, " has joined room: ", socket.room);
             } else {
-                if(rooms.indexOf(data.room) <= 0) socket.join(data.room);
+                if (rooms.indexOf(data.room) <= 0) socket.join(data.room);
 
                 socket.room = data.room;
                 socket.pid = 1;
                 socket.hash = generateHash(8);
                 socket.username = socket.handshake.query.username;
                 socket.email = socket.handshake.query.email;
-                
+
                 gameLogic.games[data.room] = {
                     player1: socket,
                     moves: 0,
-                    board: [[0,0,0,0,0,0,0],
-                            [0,0,0,0,0,0,0],
-                            [0,0,0,0,0,0,0],
-                            [0,0,0,0,0,0,0],
-                            [0,0,0,0,0,0,0],
-                            [0,0,0,0,0,0,0]]
+                    board: [[0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0]]
                 };
                 rooms.push(data.room);
-                socket.emit("assign", {pid: socket.pid, hash: socket.hash});
-                console.log("Player 1 ", socket.username ," has joined room: ", socket.room);
+                socket.emit("assign", { pid: socket.pid, hash: socket.hash });
+                console.log("Player 1 ", socket.username, " has joined room: ", socket.room);
             }
-    
-            socket.on('makeMove', function(data){ 
+
+            socket.on('makeMove', function (data) {
                 var game = gameLogic.games[socket.room];
-                if(data.hash = socket.hash && game.turn === socket.pid){
+                if (data.hash = socket.hash && game.turn === socket.pid) {
                     var move_made = gameLogic.make_move(socket.room, data.col, socket.pid);
 
-                    if(move_made){
-                        game.moves = parseInt(game.moves)+1;
-                        socket.broadcast.to(socket.room).emit('move_made', {pid: socket.pid, col: data.col});
+                    if (move_made) {
+                        game.moves = parseInt(game.moves) + 1;
+                        socket.broadcast.to(socket.room).emit('move_made', { pid: socket.pid, col: data.col });
                         game.turn = socket.opponent.pid;
                         var winner = gameLogic.check_for_win(game.board);
 
@@ -147,21 +147,21 @@ mongoose.connect("mongodb://localhost:27017/usersDB", {
                             io.in(socket.room).emit("winner", {winner: winner});
                             delete gameLogic.games[socket.room];
                         }
-                        if(game.moves >= 42) {
+                        if (game.moves >= 42) {
                             io.in(socket.room).emit("draw");
                             delete gameLogic.games[socket.room];
                         }
                     }
                 }
             });
-    
-            socket.on('my_move', function(data) {
-                socket.broadcast.to(socket.room).emit('opponent_move', {col: data.col});
+
+            socket.on('my_move', function (data) {
+                socket.broadcast.to(socket.room).emit('opponent_move', { col: data.col });
             })
-    
+
             socket.on("disconnect", function () {
                 console.log("Client disconnected. ID: ", socket.id);
-                if(socket.room in gameLogic.games && typeof gameLogic.games[socket.room].player2 != "undefined") {
+                if (socket.room in gameLogic.games && typeof gameLogic.games[socket.room].player2 != "undefined") {
                     io.in(socket.room).emit("stop");
                     console.log("Room closed: " + socket.room);
                     delete gameLogic.games[socket.room];
--- public/JS/script.js
+++ public/JS/script.js
@@ -1,196 +1,196 @@
 const OGurl = window.location.origin;
-let socket = io.connect(OGurl, { query: {email: user_email, username: user_name} });
+let socket = io.connect(OGurl, { query: { email: user_email, username: user_name } });
 
 var player = {},
 	yc = $('.your_color'),
 	oc = $('.opponent_color'),
 	your_turn = false,
 	url = window.location.href.split('/'),
-	room = url[url.length-1];
+	room = url[url.length - 1];
 
-	var text = {
-		'yt' : "Your turn",
-		'nyt' : "Waiting for opponent",
-		'popover_h2' : "Waiting for opponent",
-		'popover_p' : "Give the url to a friend to play a game",
-		'popover_h2_win' : "You won the game!",
-		'popover_p_win' : "Give the url to a friend to play another game",
-		'popover_h2_lose' : "You lost the game..",
-		'popover_p_lose' : "Give the url to a friend to play another game",
-		'popover_h2_draw' : "Its a draw.. bummer!",
-		'popover_p_draw' : "Give the URL to a friend to play another game",
-		'popover_h2_another_session' : "Not allowed",
-		'popover_p_another_session' : "You cannot play a game against yourself"
-	}
+var text = {
+	'yt': "Your turn",
+	'nyt': "Waiting for opponent",
+	'popover_h2': "Waiting for opponent",
+	'popover_p': "Give the url to a friend to play a game",
+	'popover_h2_win': "You won the game!",
+	'popover_p_win': "Give the url to a friend to play another game",
+	'popover_h2_lose': "You lost the game..",
+	'popover_p_lose': "Give the url to a friend to play another game",
+	'popover_h2_draw': "Its a draw.. bummer!",
+	'popover_p_draw': "Give the URL to a friend to play another game",
+	'popover_h2_another_session': "Not allowed",
+	'popover_p_another_session': "You cannot play a game against yourself"
+}
 
-	init();
+init();
 
-	socket.on('assign', function(data) {
-		player.pid = data.pid;
-		player.hash = data.hash;
-		if(player.pid == "1"){
-			yc.addClass('red');
-			oc.addClass('yellow');
-			player.color = 'red';
-			player.oponend = 'yellow';
-			$('.underlay').removeClass('hidden');
-			$('#Modal').modal('show');
-		}else{
-			$('.status').html(text.nyt);
-			yc.addClass('yellow');
-			oc.addClass('red');
-			oc.addClass('show');
-			player.color = 'yellow';
-			player.oponend = 'red';
-		}
-	});
-
-	socket.on('winner', function(data) {
-		oc.removeClass('show');
-		yc.removeClass('show');
-		change_turn(false);
-		for(var i = 0; i < 4; i++){
-			$('.cols .col .coin#coin_'+data.winner.winner_coins[i]).addClass('winner_coin');
-		}
-
-		if(data.winner.winner == player.pid){
-			$('#popover-title').html(text.popover_h2_win);
-			$('#popover-info').html(text.popover_p_win);
-		}else{
-			$('#popover-title').html(text.popover_h2_lose);
-			$('#popover-info').html(text.popover_p_lose);
-		}
-		
-		setTimeout(function(){
-			$('.underlay').removeClass('hidden');
-			$('#Modal').modal('show');
-		},2000);
-	});
-
-	socket.on('draw', function() {
-		oc.removeClass('show');
-		yc.removeClass('show');
-		change_turn(false);
-		$('#popover-title').html(text.popover_h2_draw);
-		$('#popover-info').html(text.popover_p_draw);
-		setTimeout(function(){
-			$('.underlay').removeClass('hidden');
-			$('#Modal').modal('show');
-		},2000);
-	});
-
-	socket.on('start', function(data) {
-		change_turn(true);
-		yc.addClass('show');
-		$('.underlay').addClass('hidden');
-		$('#Modal').modal('hide');
-	});
-
-	socket.on('assign_names', function(data) {
-		$('#player1').html(data.player1);
-		$('#player2').html(data.player2);
-	})
-
-	socket.on('stop', function(data) {
-		init();
-		reset_board();
-
-		oc.removeClass('show');
-		yc.removeClass('show');
-
-		$('#popover-title').html(text.popover_h2_win);
-		$('#popover-info').html(text.popover_p_win);
-
-		setTimeout(function(){
-			$('.underlay').removeClass('hidden');
-			$('#Modal').modal('show');
-		},500);
-	});
-
-	socket.on('move_made', function(data) {
-		make_move(data.col+1, true);
-		change_turn(true);
-		yc.addClass('show');
-		oc.removeClass('show');
-	});
-
-	socket.on('opponent_move', function(data) {
-		if(!your_turn){
-			oc.css('left', parseInt(data.col)*100);
-		}
-		console.debug(data);
-	});
-
-	socket.on("not_allowed", function(data) {
-		$('#popover-title').html(text.popover_h2_another_session);
-		$('#popover-info').html(text.popover_p_another_session);
+socket.on('assign', function (data) {
+	player.pid = data.pid;
+	player.hash = data.hash;
+	if (player.pid == "1") {
+		yc.addClass('red');
+		oc.addClass('yellow');
+		player.color = 'red';
+		player.oponend = 'yellow';
 		$('.underlay').removeClass('hidden');
 		$('#Modal').modal('show');
-		$('.share_url').hide();
-		setTimeout(function(){
-			window.location.href = "/";
-		}, 3000);
-	});
+	} else {
+		$('.status').html(text.nyt);
+		yc.addClass('yellow');
+		oc.addClass('red');
+		oc.addClass('show');
+		player.color = 'yellow';
+		player.oponend = 'red';
+	}
+});
 
-	$('.cols > .col').mouseenter(function(){
-		if(your_turn){
-			yc.css('left', $(this).index()*100);
-			socket.emit('my_move', {col: $(this).index()});
+socket.on('winner', function (data) {
+	oc.removeClass('show');
+	yc.removeClass('show');
+	change_turn(false);
+	for (var i = 0; i < 4; i++) {
+		$('.cols .col .coin#coin_' + data.winner.winner_coins[i]).addClass('winner_coin');
+	}
+
+	if (data.winner.winner == player.pid) {
+		$('#popover-title').html(text.popover_h2_win);
+		$('#popover-info').html(text.popover_p_win);
+	} else {
+		$('#popover-title').html(text.popover_h2_lose);
+		$('#popover-info').html(text.popover_p_lose);
+	}
+
+	setTimeout(function () {
+		$('.underlay').removeClass('hidden');
+		$('#Modal').modal('show');
+	}, 2000);
+});
+
+socket.on('draw', function () {
+	oc.removeClass('show');
+	yc.removeClass('show');
+	change_turn(false);
+	$('#popover-title').html(text.popover_h2_draw);
+	$('#popover-info').html(text.popover_p_draw);
+	setTimeout(function () {
+		$('.underlay').removeClass('hidden');
+		$('#Modal').modal('show');
+	}, 2000);
+});
+
+socket.on('start', function (data) {
+	change_turn(true);
+	yc.addClass('show');
+	$('.underlay').addClass('hidden');
+	$('#Modal').modal('hide');
+});
+
+socket.on('assign_names', function (data) {
+	$('#player1').html(data.player1);
+	$('#player2').html(data.player2);
+})
+
+socket.on('stop', function (data) {
+	init();
+	reset_board();
+
+	oc.removeClass('show');
+	yc.removeClass('show');
+
+	$('#popover-title').html(text.popover_h2_win);
+	$('#popover-info').html(text.popover_p_win);
+
+	setTimeout(function () {
+		$('.underlay').removeClass('hidden');
+		$('#Modal').modal('show');
+	}, 500);
+});
+
+socket.on('move_made', function (data) {
+	make_move(data.col + 1, true);
+	change_turn(true);
+	yc.addClass('show');
+	oc.removeClass('show');
+});
+
+socket.on('opponent_move', function (data) {
+	if (!your_turn) {
+		oc.css('left', parseInt(data.col) * 100);
+	}
+	console.debug(data);
+});
+
+socket.on("not_allowed", function (data) {
+	$('#popover-title').html(text.popover_h2_another_session);
+	$('#popover-info').html(text.popover_p_another_session);
+	$('.underlay').removeClass('hidden');
+	$('#Modal').modal('show');
+	$('.share_url').hide();
+	setTimeout(function () {
+		window.location.href = "/";
+	}, 3000);
+});
+
+$('.cols > .col').mouseenter(function () {
+	if (your_turn) {
+		yc.css('left', $(this).index() * 100);
+		socket.emit('my_move', { col: $(this).index() });
+	}
+});
+
+$('.cols > .col').click(function () {
+	if (parseInt($(this).attr('data-in-col')) < 6) {
+		if (your_turn) {
+			var col = $(this).index() + 1;
+			make_move(col);
+			socket.emit('makeMove', { col: col - 1, hash: player.hash });
+			change_turn(false);
+			yc.removeClass('show');
+			oc.addClass('show');
 		}
-	});
-
-	$('.cols > .col').click(function(){
-		if(parseInt($(this).attr('data-in-col')) < 6){
-			if(your_turn){
-				var col = $(this).index()+1;
-				make_move(col);
-				socket.emit('makeMove', {col: col-1, hash: player.hash});
-				change_turn(false);
-				yc.removeClass('show');
-				oc.addClass('show');
-			}
-		}
-	});
-
-	function make_move(col, other){
-		if(!other) other = false;
-		var col_elm = $('.cols > .col#col_'+col);
-		var current_in_col = parseInt(col_elm.attr('data-in-col'));
-		col_elm.attr('data-in-col', current_in_col+1);
-		var color = (other) ? player.oponend : player.color;
-		var new_coin = $('<div class="coin '+color+'" id="coin_'+(5-current_in_col)+''+(col-1)+'"></div>');
-		col_elm.append(new_coin);
-		new_coin.animate({
-			top : 100*(4-current_in_col+1),
-		}, 400);
 	}
+});
 
-	function init(){
-		socket.emit('join', {room: room});
-		$('.share_url').val(window.location.href);
-		$('#popover-title').html(text.popover_h2);
-		$('#popover-info').html(text.popover_p);
-		$('.status').html('');
-	}
+function make_move(col, other) {
+	if (!other) other = false;
+	var col_elm = $('.cols > .col#col_' + col);
+	var current_in_col = parseInt(col_elm.attr('data-in-col'));
+	col_elm.attr('data-in-col', current_in_col + 1);
+	var color = (other) ? player.oponend : player.color;
+	var new_coin = $('<div class="coin ' + color + '" id="coin_' + (5 - current_in_col) + '' + (col - 1) + '"></div>');
+	col_elm.append(new_coin);
+	new_coin.animate({
+		top: 100 * (4 - current_in_col + 1),
+	}, 400);
+}
 
-	function reset_board(){
-		$('.cols .col').attr('data-in-col', '0').html('');
-		yc.removeClass('yellow red');
-		oc.removeClass('yellow red');
-		yc.removeClass('show');
-		oc.removeClass('show');
-	}
+function init() {
+	socket.emit('join', { room: room });
+	$('.share_url').val(window.location.href);
+	$('#popover-title').html(text.popover_h2);
+	$('#popover-info').html(text.popover_p);
+	$('.status').html('');
+}
 
-	function change_turn(yt){
-		if(yt){
-			your_turn = true;
-			$('.status').html(text.yt);
-		}else{
-			your_turn = false;
-			$('.status').html(text.nyt);
-		}
+function reset_board() {
+	$('.cols .col').attr('data-in-col', '0').html('');
+	yc.removeClass('yellow red');
+	oc.removeClass('yellow red');
+	yc.removeClass('show');
+	oc.removeClass('show');
+}
+
+function change_turn(yt) {
+	if (yt) {
+		your_turn = true;
+		$('.status').html(text.yt);
+	} else {
+		your_turn = false;
+		$('.status').html(text.nyt);
 	}
-	
-	$('.share_url').click(function(){
-		 $(this).select();
-	});
+}
+
+$('.share_url').click(function () {
+	$(this).select();
+});
